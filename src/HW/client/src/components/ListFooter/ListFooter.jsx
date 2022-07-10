5b2fe523ea1c897c4ce2d1ee1e0fbd954fc00d8e
Orihassid <Orihassid123@gmail.com>
2022-07-10 10:19:03 +0300

    refactor:numOfItems redux

--- src/HW/client/src/components/ListFooter/ListFooter.jsx
+++ src/HW/client/src/components/ListFooter/ListFooter.jsx
@@ -1,26 +1,22 @@
+import "./ListFooter.css";
+import PropTypes from "prop-types";
+const ListFooter = ({ numOfItems, clearAllItemsAction }) => {
+  const handleClearAllClick = async () => {
+    await clearAllItemsAction();
+  };
 
-import "./ListFooter.css"
-import PropTypes from 'prop-types';
-const ListFooter = ({numOfItems,clearAllItemsAction})=>{
+  return (
+    <footer>
+      <p className="count"> Item(s): {numOfItems} </p>
+      <button onClick={handleClearAllClick} className="clearAllBtn">
+        Clear All
+      </button>
+    </footer>
+  );
+};
+ListFooter.propTypes = {
+  numOfItems: PropTypes.number,
+  clearAllItemsAction: PropTypes.func,
+};
 
-
-    const handleClearAllClick = async () => {
-        
-        await clearAllItemsAction();
-      };
-
-    return (
-        <footer>
-           {numOfItems>0 && <p  className="count"> Item(s): {numOfItems} </p>}
-            <button onClick={handleClearAllClick} className="clearAllBtn">Clear All</button>
-          </footer>
-    )
-}
-ListFooter.propTypes={
-    numOfItems:PropTypes.number,
-    clearAllItemsAction:PropTypes.func
-
-}
-
-
-export default ListFooter
+export default ListFooter;
