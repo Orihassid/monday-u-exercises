70ddb4f68c1a0168be2fb378d1d6311f3a1dc8d8
Orihassid <Orihassid123@gmail.com>
2022-07-22 18:26:08 +0300

    feat: heroku deployment

--- src/HW/client/src/components/List/__tests__/List.test.jsx
+++ src/HW/client/src/components/List/__tests__/List.test.jsx
@@ -1,8 +1,8 @@
 import List from "../List";
 import { render, screen } from "@testing-library/react";
- import { Provider } from "react-redux";
- import { store } from "../../../store";
- import AppContainer from "../../AppContainer/AppContainer";
+import { Provider } from "react-redux";
+import { store } from "../../../store";
+import AppContainer from "../../AppContainer/AppContainer";
 
 const items = [
   {
@@ -26,28 +26,27 @@ const items = [
 describe("List", () => {
   test("should render both items (one done and one not)", () => {
     render(
-        <Provider store={store}>
-    <List items={items} searchInputValue={''} />;
-    </Provider>)
-     const shoppingTodo = screen.getByTestId(`item-${items[0].itemId}`);
-     expect(shoppingTodo).toBeInTheDocument();
-     const checkbox = screen.getAllByRole('checkbox');
-     expect(checkbox[0]).not.toBeChecked()
- 
-     const beachTodo = screen.getByTestId(`item-${items[0].itemId}`);
-     expect(beachTodo).toBeInTheDocument();
-     expect(checkbox[1]).toBeChecked()
+      <Provider store={store}>
+        <List items={items} searchInputValue={""} />;
+      </Provider>
+    );
+    const shoppingTodo = screen.getByTestId(`item-${items[0].itemId}`);
+    expect(shoppingTodo).toBeInTheDocument();
+    const checkbox = screen.getAllByRole("checkbox");
+    expect(checkbox[0]).not.toBeChecked();
+
+    const beachTodo = screen.getByTestId(`item-${items[0].itemId}`);
+    expect(beachTodo).toBeInTheDocument();
+    expect(checkbox[1]).toBeChecked();
   });
 });
 
 test("should call fetchItems function", () => {
-    const mockGetItemsAction = jest.fn(() => items);
-    render(
-      <Provider store={store}>
-        <AppContainer items={items} getItemsAction={mockGetItemsAction} />
-      </Provider>
-    );
-    expect(mockGetItemsAction).toHaveBeenCalled();
-  });
-
-
+  const mockGetItemsAction = jest.fn(() => items);
+  render(
+    <Provider store={store}>
+      <AppContainer items={items} getItemsAction={mockGetItemsAction} />
+    </Provider>
+  );
+  expect(mockGetItemsAction).toHaveBeenCalled();
+});
